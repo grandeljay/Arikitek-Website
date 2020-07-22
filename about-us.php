a7aa9dbc824c88f60f71c248e2c48e1df21bcf24
Jay <j.trees@hybridsupply.de>
2020-07-22 15:49:38 +0200

    Allow more than 1 email link per page

--- about-us.php
+++ about-us.php
@@ -8,10 +8,10 @@ $_GLOBALS['file'] = basename(__FILE__);
 
 <head>
     <title>About us - Arikitek - creators of OpsApp</title>
-    
+
     <?php
     include_once 'inc/head.php';
-    ?>    
+    ?>
 </head>
 
 <body>
@@ -27,15 +27,15 @@ $_GLOBALS['file'] = basename(__FILE__);
                 ?>
             </nav>
         </header>
-        
-        <div id="main">                     
+
+        <div id="main">
           <main>
-              <h1>About us</h1>     
-              
+              <h1>About us</h1>
+
               <?php
               include_once 'inc/compatibility.php';
               ?>
-               
+
               <section class="skew">
                   <p>We are a small team of professionals with over 12 years experience in the telecommunications industry. We employ staff with ethical and value-oriented work principles and rise to meet the challenge of a diverse workforce. We partner with innovative networks and work jointly with project teams to maximize client benefits.</p>
                   <p>At the forefront of our business is the expectation that we are working for the greater good of the planet. We commit to giving financially to charities that specifically benefit the underprivileged.</p>
@@ -43,10 +43,10 @@ $_GLOBALS['file'] = basename(__FILE__);
                   <ul>
                       <li>Have Faith Projects</li>
                       <li>HOSA South Africa</li>
-                  </ul> 
+                  </ul>
               </section>
-              
-              <section class="skew">                
+
+              <section class="skew">
                   <h2>Imprint</h2>
                   <table>
                   <thead>
@@ -62,26 +62,26 @@ $_GLOBALS['file'] = basename(__FILE__);
                       <td>40489 Düsseldorf</td>
                   </tr>
                   <tr>
-                      <td><p><a id="electronicMessage" class="fallbackUnlink" data-href="aW5mb0BhcmlraXRlay5jb20=">info<span class="at"></span>arikitek<span class="dot"></span>com</a></p></td>
+                      <td><p><a class="electronicMessage fallbackUnlink" data-href="aW5mb0BhcmlraXRlay5jb20=">info<span class="at"></span>arikitek<span class="dot"></span>com</a></p></td>
                   </tr>
                   </tbody>
                   </table>
-                  
+
                   <p>Handelsregister Nr.: HRB 76414</p>
                   <p>Registergericht: Düsseldorf</p>
                   <p>Umsatzsteuer Nr.: DE 30 36 70 418</p>
               </section>
           </main>
-          
+
           <?php
           include_once 'inc/popup.php';
           ?>
-          
+
           <?php
           include_once 'inc/back-to-top.php';
-          ?>                      
+          ?>
         </div>
-        
+
         <footer>
             <?php
             include_once 'inc/footer.php';
@@ -90,4 +90,4 @@ $_GLOBALS['file'] = basename(__FILE__);
     </div>
 </body>
 
-</html> 
+</html>
--- inc/head.php
+++ inc/head.php
@@ -3,28 +3,28 @@
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+
   <!-- Basic Info -->
   <meta name="author" content="Arikitek">
   <meta name="description" content="Arikitek are the creators of OpsApp - an innovative approach to managing your smart phone Address Book.">
-  
+
   <!-- Open Graph Data -->
-  <meta property="og:title" content="Arikitek">  
+  <meta property="og:title" content="Arikitek">
   <meta property="og:description" content="Arikitek are the creators of OpsApp - an innovative approach to managing your smart phone Address Book.">
-  <meta property="og:image" content="img/opsapp-logo.png"><!-- PLACE AN ABSOLUTE PATH HERE -->  
-  
+  <meta property="og:image" content="img/opsapp-logo.png"><!-- PLACE AN ABSOLUTE PATH HERE -->
+
   <!-- favicon -->
   <meta name="apple-mobile-web-app-title" content="Arikitek">
   <meta name="application-name" content="Arikitek">
   <meta name="msapplication-TileColor" content="#2b5797">
-  <meta name="theme-color" content="#ffffff">    
-  
+  <meta name="theme-color" content="#ffffff">
+
   <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
   <link rel="manifest" href="site.webmanifest">
   <link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5">
-      
+
   <!-- Critical CSS -->
   <style type="text/css">
     :root {
@@ -33,66 +33,66 @@
       --colourPrimary: #24adba;
       --colourPrimaryDark: #197093;
       --colourSecondary: #5a5a5a;
-      
+
       --radii: 2px;
       --radiix2: 4px;
-      
+
       --shadowDefault:
-          1px 1px 1px rgba(100,100,100,0.12), 
-          2px 2px 2px rgba(100,100,100,0.12), 
-          4px 4px 4px rgba(100,100,100,0.12), 
-          8px 8px 8px rgba(100,100,100,0.12);      
+          1px 1px 1px rgba(100,100,100,0.12),
+          2px 2px 2px rgba(100,100,100,0.12),
+          4px 4px 4px rgba(100,100,100,0.12),
+          8px 8px 8px rgba(100,100,100,0.12);
       --shadowDefaultInset:
-          1px 1px 1px rgba(100,100,100,0.12) inset, 
-          2px 2px 2px rgba(100,100,100,0.12) inset, 
-          4px 4px 4px rgba(100,100,100,0.12) inset, 
-          8px 8px 8px rgba(100,100,100,0.12) inset;                
+          1px 1px 1px rgba(100,100,100,0.12) inset,
+          2px 2px 2px rgba(100,100,100,0.12) inset,
+          4px 4px 4px rgba(100,100,100,0.12) inset,
+          8px 8px 8px rgba(100,100,100,0.12) inset;
       --shadowSidebar:
-          1px 0 1px rgba(100,100,100,0.12), 
-          2px 0 2px rgba(100,100,100,0.12), 
-          4px 0 4px rgba(100,100,100,0.12), 
+          1px 0 1px rgba(100,100,100,0.12),
+          2px 0 2px rgba(100,100,100,0.12),
+          4px 0 4px rgba(100,100,100,0.12),
           8px 0 8px rgba(100,100,100,0.12);
       --shadowTopbar:
-          0 1px 1px rgba(100,100,100,0.12), 
-          0 2px 2px rgba(100,100,100,0.12), 
+          0 1px 1px rgba(100,100,100,0.12),
+          0 2px 2px rgba(100,100,100,0.12),
           0 4px 4px rgba(100,100,100,0.12);
-      --shadowDrop: var(--radii) var(--radii) var(--radiix2) rgba(100,100,100,0.4); 
+      --shadowDrop: var(--radii) var(--radii) var(--radiix2) rgba(100,100,100,0.4);
     }
 
     body {
       margin: 0;
     }
-    
+
     .wrapper {
       display: grid;
       grid-template-columns: 1fr 3fr;
       grid-template-rows: 1fr max-content;
-                  
+
       min-height: 100vh;
       font-size: 18pt;
     }
-    
+
     img {
       max-width: 100%;
-    }        
-    
+    }
+
     h1 {
       margin: 4rem;
     }
-    
+
     header {
       grid-row: 1 / 2;
       grid-column: 1 / 2;
-      
+
       color: var(--colourWhite);
-      background-color: var(--colourPrimaryDark);            
+      background-color: var(--colourPrimaryDark);
     }
-                    
-    header #logo {        
+
+    header #logo {
       padding: 24px;
       font-size: 2em;
-      
-      background-color: #f4f4f4;            
+
+      background-color: #f4f4f4;
     }
 
     header #logo .ariki {
@@ -101,15 +101,15 @@
 
     header #logo .tek {
       color: var(--colourSecondary);
-    }    
-    
+    }
+
     header nav ul {
       margin: 0;
       padding: 0;
 
       list-style-type: none;
-    }        
-                
+    }
+
     header nav > ul li a {
       display: block;
       padding: 24px;
@@ -123,89 +123,89 @@
       height: 24px;
       margin-right: 8px;
       margin-bottom: 2px;
-    }    
-    
+    }
+
     #main {
       grid-row: 1 / 3;
       grid-column: 2 / 3;
-      
+
       background-color: #f9fafb;
     }
-    
+
     aside,
     section {
       padding: 1rem;
-    }        
-    
-    .skew {
-      margin: 4rem 0;
-      padding: 4rem;            
     }
 
-    a#electronicMessage .at::before {
+    .skew {
+      margin: 4rem 0;
+      padding: 4rem;
+    }
+
+    a.electronicMessage .at::before {
       content: '@';
     }
 
-    a#electronicMessage .dot::before {
+    a.electronicMessage .dot::before {
       content: '.';
-    }    
-    
+    }
+
     form {
       display: grid;
       grid-template-columns: 1fr;
-      grid-gap: 1em;  
-    }    
-    
+      grid-gap: 1em;
+    }
+
     fieldset {
       padding: 0;
       margin: 0;
-      
+
       border: none;
     }
-    
+
     fieldset div {
       display: grid;
       grid-template-columns: minmax(max-content, 12vw) auto;
       grid-template-rows: auto auto;
       grid-gap: 0.2em 1em;
     }
-    
-    fieldset div legend {      
+
+    fieldset div legend {
       grid-column: 2 / 3;
       grid-row: 2 / 3;
     }
-    
+
     #bodyFooter {
 	    display: grid;
 	    grid-gap: 1rem;
 	    grid-template-columns: max-content max-content;
 	    justify-content: flex-end;
-	    
+
 	    padding: 0 4rem 2rem 4rem;
-    }    
-    
+    }
+
     footer {
       grid-column: 1 / 2;
       grid-row: 2 / 3;
-      
+
       padding: 1em;
       font-size: 12pt;
-      
+
       color: var(--colourWhite);
-      background-color: var(--colourBlack);            
-    }    
-    
+      background-color: var(--colourBlack);
+    }
+
     #backToTopAnchor,
     #backToMenuAnchor,
     #imagePreview,
     #outdatedBrowser {
       display: none;
-    }        
-    
+    }
+
     #backToMenuAnchor {
       opacity: 0;
     }
-    
+
     @media(max-width: 1280px) {
       .wrapper {
         grid-template-columns: 1fr 2fr;
@@ -217,24 +217,24 @@
         grid-template-columns: 1fr;
         grid-template-rows: auto auto auto;
       }
-      
+
       fieldset div legend {
         grid-column: auto;
         grid-row: 3 / 4;
       }
-      
+
       fieldset div p,
       fieldset div button {
         grid-column-start: 1;
       }
-      
+
       button {
         max-width: unset;
       }
-      
+
       label {
         padding: 0;
-      }      
+      }
     }
 
     @media(max-width: 768px) {
@@ -242,59 +242,59 @@
         grid-template-columns: 1fr;
         grid-template-rows: max-content;
       }
-      
+
       header {
         position: relative;
-        
+
         grid-row: 1 / 2;
       }
-      
-      header #logo {        
+
+      header #logo {
         padding: 18px 24px;
         font-size: 1.6em;
-      }        
-      
+      }
+
       header nav {
         display: none;
       }
-      
+
       #main {
         grid-row: 2 / 3;
         grid-column: 1 / 2;
       }
-      
+
       header #logo.menu a#menuToggle {
         display: inline-block;
-      }   
-      
+      }
+
       footer {
         grid-row: 3 / 4;
         grid-column: 1 / 2;
       }
-    }    
-    
+    }
+
     @media(max-width: 512px) {
       h1 {
         margin: 1rem;
       }
-      
+
       table {
         margin: 0;
-      }        
-      
+      }
+
       .skew {
-        padding: 2rem 1rem;            
+        padding: 2rem 1rem;
       }
     }
-  </style>    
-  
+  </style>
+
   <!-- If JavaScript is enabled, defer script here -->
   <link rel="stylesheet" href="css/default.css" id="defaultCSS">
 
   <!-- Fallback to normal loading if JavaScript is not enabled -->
   <noscript>
   <link rel="stylesheet" href="css/default.css">
-  </noscript>  
-  
+  </noscript>
+
   <!-- Defer JavaScript so the DOM can continue loading -->
   <script defer src="js/default.js"></script>
